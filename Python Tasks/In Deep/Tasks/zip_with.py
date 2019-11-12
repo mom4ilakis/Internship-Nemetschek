8edc8017b2769e8cea4f3e28179bd2aacc437b38
Momchil Marinov <MVMarinov@nemetschek.bg>
2019-11-12 15:45:45 +0200

    Cleaned the code

--- Python Tasks/In Deep/Tasks/zip_with.py
+++ Python Tasks/In Deep/Tasks/zip_with.py
@@ -1,17 +1,14 @@
 def zip_with(func, *iterables):
-    #if iterables is empty
+    # if iterables is empty
     if len(iterables) == 0:
         yield list()
-    #finding the length of the shortest iterable
-    shortest = len(iterables[0])
-    for obj in iterables:
-        if shortest > len(obj):
-            shortest = len(obj)
-    #list of arguments to unpack for func
+    # finding the length of the shortest iterable
+    shortest = len(min(iterables, key=len))
+    # list of arguments to unpack for func
     args = list()
 
     for i in range(0, shortest):
-        #clearing argument list
+        # clearing argument list
         args.clear()
         for obj in iterables:
             args.append(obj[i])
@@ -22,16 +19,13 @@ def concat3(x, y, z):
     return x + y + z
 
 
-first_names = ['John', 'Miles'] 
-last_names = ['Coltrane', 'Davis'] 
+def concat_many(*args):
+    res = ""
+    return [res + i for i in args]
+
+
+first_names = ['John', 'Miles', "Doe"]
+last_names = ['Coltrane', 'Davis']
 spaces = [' '] * 2
 
-print(list(zip_with(concat3, first_names, spaces, last_names)))
-
-
-
-
-
-
-
-
+print(list(zip_with(concat_many, first_names, spaces, last_names)))
