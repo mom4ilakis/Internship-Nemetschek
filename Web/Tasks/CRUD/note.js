c40fa69f16ff34a8d18cbc14f61da8d3801b7fa2
Momchil Marinov <MVMarinov@nemetschek.bg>
2019-12-12 15:17:39 +0200

    moved functions from utils

--- Web/Tasks/CRUD/note.js
+++ Web/Tasks/CRUD/note.js
@@ -1,11 +1,39 @@
 function uuid() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
     });
-  }
+}
+
+function debounce(func, wait, immidiate) {
+    let timeout;
+
+    return function () {
+        let context = this;
+        let args = arguments;
+        let callNow = immidiate && !timeout;
+
+        clearTimeout(timeout);
+
+        timeout = setTimeout(() => {
+            timeout = null;
+
+            if (!immidiate) {
+                func.apply(context, args);
+            }
+        }, wait);
+        if (callNow) {
+            func.apply(context, args);
+        }
+    };
+}
+
+if (!window) {
+    var notesAPI = {};
+}
+
 class Note {
-    constructor(title, body, id=uuid()) {
+    constructor(title, body, id = uuid()) {
         this.title = title;
         this.body = body;
         this.ID = id;
@@ -22,11 +50,11 @@ class Note {
     }
 
     save() {
-        debounce(notesAPI.saveNote(this));
+        debounce(window.notesAPI.saveNote(this));
     }
 
     delete() {
-        notesAPI.deleteNote(this);
+        window.notesAPI.deleteNote(this);
     }
 }
 module.exports = Note;
