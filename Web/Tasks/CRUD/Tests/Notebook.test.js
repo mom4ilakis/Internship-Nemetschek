dd9c3ce6b665cd78dfc37b9de8033b2406619294
Momchil Marinov <MVMarinov@nemetschek.bg>
2019-12-18 15:21:39 +0200

    added test for load, update

--- Web/Tasks/CRUD/Tests/Notebook.test.js
+++ Web/Tasks/CRUD/Tests/Notebook.test.js
@@ -1,5 +1,6 @@
 const Note = require('../note');
 
+const uuid = require('../utils');
 const Notebook = require('../notebook');
 
 jest.mock('../note');
@@ -9,14 +10,6 @@ describe('Notebook testing', () => {
         const notebook = new Notebook();
         expect(notebook.notes).toEqual({});
     });
-
-    test('Notebokk addNote', () => {
-        const notebook = new Notebook();
-        const note = new Note();
-        notebook.addNote(note);
-        expect(notebook.notes[note.ID]).toEqual(note);
-    });
-
     describe('Notebook add/del tests', () => {
         test('addNote', () => {
             const note = new Note();
@@ -32,13 +25,55 @@ describe('Notebook testing', () => {
             notebook.deleteNote(note.ID);
             expect(note.delete).toHaveBeenCalled();
         });
-        test('deletAll', () => {
-            const notes = [new Note(), new Note(), new Note(), new Note()];
+        test('deleteAll on empty notebook', () => {
+            const notebook = new Notebook();
+            notebook.deleteNote = jest.fn();
+            notebook.deleteAll();
+            expect(notebook.deleteNote).toBeCalledTimes(0);
+        });
+        test('deleteAll', () => {
+            const notes = [new Note('t', 'b', '1'), new Note('t', 'b',  '2'), new Note('t', 'b', '3'), new Note('t', 'b', '4')];
             const notebook = new Notebook();
             notes.forEach(note => {
+                note.ID = uuid();
                 notebook.addNote(note);
             });
+            notebook.deleteNote = jest.fn();
+            notebook.deleteAll();
             expect(notebook.deleteNote).toHaveBeenCalledTimes(4);
+        });
+        test('updateNote on empty notebook', () => {
+            const note = new Note();
+            const notebook = new Notebook();
+            note.ID = uuid();
+            notebook.updateNote(note);
+            expect(notebook.notes[note.ID]).toEqual(note);
+        });
+        test('updateNote', () => {
+            const note = new Note();
+            note.ID = uuid();
+            note.title = 'title';
+            note.body = 'body';
+            const notebook = new Notebook();
+
+            notebook.addNote(note);
+
+            note.title = 'title1';
+            note.body = 'body1';
+
+            notebook.updateNote(note);
+
+            expect(notebook.notes[note.ID].title).toEqual(note.title);
+            expect(notebook.notes[note.ID].body).toEqual(note.body);
+        });
+        test('load on empty notebook', () => {
+            global.notesAPI = {
+                getNotes: jest.fn()
+                    .mockResolvedValue([new Note(), new Note(), new Note(), new Note()]),
+            };
+            const notebook = new Notebook();
+            notebook.load();
+            expect(global.notesAPI.getNotes).toHaveBeenCalled();
         });
     });
 });
