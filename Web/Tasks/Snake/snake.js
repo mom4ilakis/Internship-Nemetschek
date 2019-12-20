bcfebc0019f41c88ffbf2f58fe6ccf9ab141a04f
Momchil Marinov <MVMarinov@nemetschek.bg>
2019-12-20 13:28:07 +0200

    a skeleton of the snake obj

--- Web/Tasks/Snake/snake.js
+++ Web/Tasks/Snake/snake.js
@@ -1,27 +1,94 @@
-var Snake = function(x,y) {
-    const posX = x;
-    const posY = y;
-
-    moveUp = function() {
+var Snake = function(gameBoard, x, y) {
+    let posX = x;
+    let posY = y;
+    let energy = 1;
+    let length = 1;
+    let board = gameBoard;
+    let direction = {
+        none: 0,
+        up: 1,
+        right: 2,
+        down: 3,
+        left: 4
     };
-    moveDown = function() {
+    let currentDirection = direction.none;
 
-    };
-    moveLeft = function() {
-
-    };
-    moveRight = function() {
-
-    };
-    move = function(dir) {
-        switch(dir) {
-        case 'up' : moveUp();break;
-        case 'down': moveDown();break;
-        case 'left': moveLeft();break;
-        case 'right': moveRight();break;
+    function moveUp() {
+        if(currentDirection != direction.down){
+            currentDirection = direction.up;
+            posX-= (1 * energy);
         }
+    }
+    function moveDown() {
+        if(currentDirection != direction.up){
+            currentDirection = direction.down;
+            posX+= (1 * energy);
+        }
+    }
+    function moveLeft() {
+        if(currentDirection != direction.right){
+            currentDirection = direction.left;
+            posY-= (1 * energy);
+        }
+    }
+    function moveRight() {
+        if(currentDirection != direction.left){
+            currentDirection = direction.right;
+            posY+=(1 * energy);
+        }
+    }
+    
+    function getX() {
+        return posX;
+    }
+
+    function getY() {
+        return posY;
+    }
+
+    function getEnergy() {
+        return energy;
+    }
+
+    function changeEnergy(delta) {
+        
+        energy*= delta;
+        
+        if(energy <= 0) {
+            energy = 1;
+        }
+    }
+
+    function getLength() {
+        return length;
+    }
+
+    function changeLength(delta) {
+        length+= delta;
+        
+        if(length <= 0 ){
+            board.endGame();
+        }
+    }
+
+    function draw() {
+        //to do draw a bunch of rectangles
+        //board holds the canvas
+    }
+
+    return {
+        moveUp : moveUp,
+        moveDown: moveDown,
+        moveLeft: moveLeft,
+        moveRight: moveRight,
+        getX: getX,
+        getY: getY,
+        getEnergy: getEnergy,
+        changeEnergy: changeEnergy,
+        getLength: getLength,
+        changeLength: changeLength,
     };
+};
 
 
-
-}
+module.exports = Snake;
