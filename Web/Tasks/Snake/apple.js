de06884c64d6caf2c2701dc0ba6adfa8778d4077
Momchil Marinov <MVMarinov@nemetschek.bg>
2019-12-23 16:59:16 +0200

    added setters/getters, started collision logic

--- Web/Tasks/Snake/apple.js
+++ Web/Tasks/Snake/apple.js
@@ -1,21 +1,56 @@
-var Apple = function(gameBoard, appleEnergy, x, y) {
+var Apple = function(gameBoard, appleEnergy, x, y, appleSize = 100) {
 
     let energy = appleEnergy;
     let board = gameBoard;
     let posX = x;
     let posY = y;
+    let size = appleSize;
 
     function collide(snake) {
-        snake.changeEnergy(energy);
-        //tell board that the apple has been eated
-    }
+        const x1, x2, x3, x4;
+        const y1, y2, y3, y4;
+        x1 = posX;
+        y1 = posY;
+        x2 = posX + size;
+        y2 = posY;
+        x3 = posX + size;
+        y3 = posY + size;
+        x4 = posX;
+        x4 = posY + size;
 
+        if(x2 >= snake.getX());
+        
+
+
+    }
+    function getX() {
+        return posX;
+    }
+    function getY() {
+        return getY;
+    }
+    function setX(newX) {
+        posX = newX;
+    }
+    function setY(newY) {
+        posY = newY;
+    }
     function draw() {
-        //board holds the canvas
+        const contex = board.getContex('2d');
+        contex.fillStyle = 'red';
+        contex.fillRect(posX, posY, size, size);
     }
 
 
 
-
+    return {
+        draw : draw,
+        getX : getX,
+        getY : getY,
+        setX : setX,
+        setY : setY,
+        collide : collide,
+    };
 
 };
+module.exports = Apple;
