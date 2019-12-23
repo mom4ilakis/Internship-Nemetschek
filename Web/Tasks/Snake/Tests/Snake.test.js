076a36cf04277a20cd1040e0b4a0b63e76c52d56
Momchil Marinov <MVMarinov@nemetschek.bg>
2019-12-23 13:59:01 +0200

    added more tests

--- Web/Tasks/Snake/Tests/Snake.test.js
+++ Web/Tasks/Snake/Tests/Snake.test.js
@@ -22,27 +22,111 @@ describe('Snake player obj tests', ()=>{
             expect(snake.getEnergy()).toEqual(1*1.25);
         });
     });
+    describe('snake get/set pos', () => {
+        test('snake getX', () => {
+            const snake = new Snake('board', 0, 0);
+            expect(snake.getX()).toEqual(0);
+        });
+        test('snake getY', () => {
+            const snake = new Snake('board', 0, 0);
+            expect(snake.getY()).toEqual(0);
+        });
+        test('snake setX', () =>{
+            const snake = new Snake('board', 0, 0);
+            snake.setX(123);
+            expect(snake.getX()).toEqual(123);
+            expect(snake.getY()).toEqual(0);
+        });
+        test('snake setY', () => {
+            const snake = new Snake('board', 0, 0);
+            snake.setY(123);
+            expect(snake.getX()).toEqual(0);
+            expect(snake.getY()).toEqual(123);
+        });
+    });
     describe('snake movement tests', () => {
         describe('basic movement', () => {
             test('snake moving up', () => {
                 const snake = new Snake('board', 0, 0);
                 snake.moveUp();
                 expect(snake.getX()).toEqual(-1);
+                expect(snake.getY()).toEqual(0);
             });
             test('snake moving down', () =>{
                 const snake = new Snake('board', 0, 0);
                 snake.moveDown();
                 expect(snake.getX()).toEqual(1);
+                expect(snake.getY()).toEqual(0);
             });
             test('snake moving left',() =>{
                 const snake = new Snake('board', 0, 0);
                 snake.moveLeft();
+                expect(snake.getX()).toEqual(0);
                 expect(snake.getY()).toEqual(-1);
             });
             test('snake moving right', () => {
                 const snake = new Snake('board', 0, 0);
                 snake.moveRight();
+                expect(snake.getX()).toEqual(0);
                 expect(snake.getY()).toEqual(1);
+            });
+        });
+        describe('energetic movement' , () => {
+            test('snake moving up, higher energy', () => {
+                const snake = new Snake('board', 0, 0);
+                snake.changeEnergy(1.25);
+                snake.moveUp();
+                expect(snake.getX()).toEqual(-1.25);
+                expect(snake.getY()).toEqual(0);
+            });
+            test('snake moving up, lower energy', () => {
+                const snake = new Snake('board',0,0);
+                snake.changeEnergy(0.25);
+                snake.moveUp();
+                expect(snake.getX()).toEqual(-0.25);
+                expect(snake.getY()).toEqual(0);
+            });
+            test('snake moving down, higher energy', () => {
+                const snake = new Snake('board', 0, 0);
+                snake.changeEnergy(1.25);
+                snake.moveDown();
+                expect(snake.getX()).toEqual(1.25);
+                expect(snake.getY()).toEqual(0);
+            });
+            test('snake moving down, lower energy', () => {
+                const snake = new Snake('board', 0, 0);
+                snake.changeEnergy(0.25);
+                snake.moveDown();
+                expect(snake.getX()).toEqual(0.25);
+                expect(snake.getY()).toEqual(0);
+            });
+            test('snake moving left, high energy', () => {
+                const snake = new Snake('board', 0, 0);
+                snake.changeEnergy(1.25);
+                snake.moveLeft();
+                expect(snake.getX()).toEqual(0);
+                expect(snake.getY()).toEqual(-1.25);
+            });
+            test('snake moving left, lower energy', () => {
+                const snake = new Snake('board', 0, 0);
+                snake.changeEnergy(0.25);
+                snake.moveLeft();
+                expect(snake.getX()).toEqual(0);
+                expect(snake.getY()).toEqual(-0.25);
+            });
+            test('snake moving right, higher energy', () => {
+                const snake = new Snake('board', 0, 0);
+                snake.changeEnergy(1.25);
+                snake.moveRight();
+                expect(snake.getX()).toEqual(0);
+                expect(snake.getY()).toEqual(1.25);
+            });
+            test('snake moving right, lower energy', () => {
+                const snake = new Snake('board', 0, 0);
+                snake.changeEnergy(0.25);
+                snake.moveRight();
+                expect(snake.getX()).toEqual(0);
+                expect(snake.getY()).toEqual(0.25);
             });
         });
     });
