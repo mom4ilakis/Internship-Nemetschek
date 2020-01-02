ec72be3efb7a7b7f0cc4d0ae21f19e879d08de07
Momchil Marinov <mom4ilakis@gmail.com>
2020-01-02 17:24:09 +0200

    init commit

---
+++ Web/Tasks/Snake/square.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-var */
+var Square = function (posX, posY, squareSize) {
+    let x = posX;
+    let y = posY;
+    let size = squareSize;
+
+    function collision(otherSquare) {
+        if (x < otherSquare.getX() + otherSquare.getSize()
+            && x + size > otherSquare.getX()
+            && y < otherSquare.getX() + otherSquare.getSize()
+            && y + size > otherSquare.getY()) {
+            return true;
+        }
+        return false;
+    }
+
+    function getX() {
+        return x;
+    }
+    function getY() {
+        return y;
+    }
+    function getSize() {
+        return size;
+    }
+    function setX(newX) {
+        x = newX;
+    }
+    function setY(newY) {
+        y = newY;
+    }
+    function setSize(newSize) {
+        size = newSize;
+    }
+
+    return {
+        getSize : getSize,
+        getX : getX,
+        getY : getY,
+        setX : setX,
+        setY : setY,
+        setSize : setSize,
+        collision : collision,
+    };
+};
+module.exports = Square;
