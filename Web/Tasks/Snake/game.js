74308e7442e51c60c525e340f88c6b8689facd76
Momchil Marinov <mom4ilakis@gmail.com>
2020-01-30 15:56:56 +0200

    added onWin function, changed gameloop

--- Web/Tasks/Snake/game.js
+++ Web/Tasks/Snake/game.js
@@ -1,8 +1,11 @@
+const baseScore = 25;
+
 class Game {
     constructor() {
         this.canvas = document.getElementById('gameField');
-        this.board = new Board(this.canvas);
+        this.board = new Board(this.canvas, this);
         this.paused = false;
+        this.score = 0;
         this.framesToSkip = 20;
         this.framesCount = 0;
         this.pause = this.pause.bind(this);
@@ -10,31 +13,70 @@ class Game {
         this.gameLoop = this.gameLoop.bind(this);
         this.setSnakeDirection = this.setSnakeDirection.bind(this);
         this.restart = this.restart.bind(this);
+        this.unpause = this.unpause.bind(this);
+        this.keyInput = this.keyInput.bind(this);
+        this.onDeath = this.onDeath.bind(this);
         document.getElementById('startBtn').addEventListener('click', this.start);
-        window.addEventListener('keydown', this.setSnakeDirection);
+        window.addEventListener('keydown', this.keyInput);
+    }
+
+    draw() {
+        this.board.draw();
+    }
+
+    keyInput(event) {
+        switch (event.keyCode) {
+        case 32:// Space
+            if (this.paused) {
+                this.unpause();
+            } else {
+                this.pause();
+            }
+            break;
+        case 13: // Enter
+            this.restart();
+            break;
+        default:
+            if (!this.paused) {
+                this.board.handleMovement(event);
+            }
+            break;
+        }
     }
 
     setSnakeDirection(event) {
         this.board.handleMovement(event);
     }
 
+    unpause() {
+        this.paused = false;
+        document.getElementById('contBtn').removeEventListener('click', this.unpause);
+    }
+
+    createContinueButton() {
+        const contBtn = document.getElementById('contBtn') || document.createElement('button');
+        contBtn.setAttribute('id', 'contBtn');
+        contBtn.innerHTML = 'Continue';
+        contBtn.addEventListener('click', this.unpause);
+        document.getElementById('area').append(contBtn);
+    }
+
     pause() {
         this.paused = true;
+        this.createContinueButton();
     }
 
     restart() {
-        this.board.snake = new Snake(this.canvas, 144, 144, 30);
+        this.board.spawnSnake();
         this.board.placeNewApple();
+        this.board.clearCanvas();
         this.paused = false;
+        this.score = 0;
+        this.framesToSkip = 20;
+        window.addEventListener('keydown', this.keyInput);
     }
 
-    onDeath() {
-        this.board.clearCanvas();
-        const context = this.canvas.getContext('2d');
-        context.fillStyle = 'red';
-        context.font = 'bold 48px Arial';
-        context.fillText('You died!', this.canvas.width / 2, this.canvas.height / 2);
-        this.pause();
+    createRestartButton() {
         const restartBtn = document.getElementById('restartBtn') || document.createElement('button');
         restartBtn.setAttribute('id', 'restartBtn');
         restartBtn.addEventListener('click', this.restart);
@@ -42,17 +84,29 @@ class Game {
         document.getElementById('area').append(restartBtn);
     }
 
-    notify(msg) {
-        switch (msg) {
-        case 'ateApple':
-            this.framesToSkip -= this.board.snake.length;
-            break;
-        case 'death':
-            this.onDeath();
-            break;
-        default:
-            break;
-        }
+    onWin() {
+        this.paused = true;
+        this.board.clearCanvas();
+        const context = this.canvas.getContext('2d');
+        context.fillStyle = 'gold';
+        context.font = 'bold 50px Arial';
+        context.fillText('You win!', this.canvas.width / 2, this.canvas.height / 2);
+        this.createRestartButton();
+    }
+
+    onDeath() {
+        this.board.clearCanvas();
+        const text = `You died! Score: ${this.score}`;
+        const context = this.canvas.getContext('2d');
+        context.fillStyle = 'red';
+        context.font = 'bold 20px Arial';
+        context.fillText(text, this.canvas.width / 2, this.canvas.height / 2);
+        this.paused = true;
+        this.createRestartButton();
+    }
+
+    speedUp() {
+        this.framesToSkip *= 0.96;
     }
 
     start() {
@@ -63,19 +117,20 @@ class Game {
         document.getElementById('area').append(pauseBnt);
         const startBtn = document.getElementById('startBtn');
         startBtn.removeEventListener('click', this.start);
-        this.board.snake.subscribe(this);
         this.gameLoop();
     }
 
-    gameLoop(timestamp) {
+    gameLoop() {
         if (!this.paused) {
-            if (this.framesCount === this.framesToSkip) {
+            this.draw();
+            if (this.framesCount >= this.framesToSkip) {
+                const multiplier = this.board.snake.length - 1;
+                this.score += multiplier * baseScore;
                 this.board.updateBoardObjects();
                 this.framesCount = 0;
             }
         }
-        this.framesCount++;
-        document.getElementById('collDetec').innerHTML = `${this.framesCount}\n${this.paused}`;
+        ++this.framesCount;
         window.requestAnimationFrame(this.gameLoop);
     }
 }
