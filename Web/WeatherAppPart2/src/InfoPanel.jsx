149a6059bb742cc64109effcfc4691a536073039
Momchil Marinov <mom4ilakis@gmail.com>
2020-02-06 15:59:44 +0200

    Passing data as props to child components

--- Web/WeatherAppPart2/src/InfoPanel.jsx
+++ Web/WeatherAppPart2/src/InfoPanel.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import TemperatureInfo from './TemperatureInfo';
 import QuickInfo from './QuckInfo';
 import WindInfo from './WindInfo';
@@ -8,12 +9,15 @@ class InfoPanel extends React.Component {
     render () {
         return (
             <div>
-                <TemperatureInfo/>
-                <QuickInfo/>;
-                <WindInfo/>;
-                <Precpitation/>
+                <TemperatureInfo data={this.props.data}/>
+                <QuickInfo data={this.props.data}/>
+                <WindInfo data={this.props.data}/>
+                <Precpitation data={this.props.data}/>
             </div>
         );
     }
 }
+InfoPanel.propTypes = {
+    data: PropTypes.object
+};
 export default InfoPanel;
