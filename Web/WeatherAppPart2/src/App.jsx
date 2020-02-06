df46e372727b7ab129bb73c4ff42f890b7e04f7b
Momchil Marinov <mom4ilakis@gmail.com>
2020-02-06 16:03:44 +0200

    Added handler for user input, added function to load from api

--- Web/WeatherAppPart2/src/App.jsx
+++ Web/WeatherAppPart2/src/App.jsx
@@ -1,19 +1,53 @@
 import React from 'react';
+import api from './api';
+import InfoPanel from './InfoPanel';
+import { formatData } from './utils.js';
 
 class App extends React.Component {
     constructor (props) {
         super(props);
-        this.state.data = null;
+        this.state = {
+            searched: false,
+            data: { wind: {}, precipation: {} }
+        };
+    }
+
+    componentDidMount () {
+
+    }
+
+    loadData = (location = 'Sofia') => {
+        api.get(location)
+            .then(res => this.setState({ data: formatData(res) }))
+            .catch(err => console.error(err));
+    }
+
+    handleSearchForLocation = event => {
+        if (event.which === 13) {
+            this.loadData(event.target.value);
+            this.setState({ searched: true });
+        }
+    }
+
+    info () {
+        if (this.state.data === null) {
+            return <p/>;
+        } else {
+            return <InfoPanel data={this.state.data}/>;
+        }
     }
 
     render () {
         return (
             <div>
-                <QuickInfo condition='Snow' description='foggy'/>
-                <TemperatureInfo maxTemp='10' minTemp='-4' currentTemp='3' feelsLikeTemperature='4' />
-                <WindInfo windSpeed='35' windDirection='west' unit='Km/h'/>
-                <Precipitation type='Snow' amount='12'/>
+                <input type='text' id='location' onKeyPress={this.handleSearchForLocation}/>
+                {
+                    this.state.searched &&
+                    <InfoPanel data={this.state.data}/>
+                }
             </div>
         );
     }
 }
+
+export default App;
