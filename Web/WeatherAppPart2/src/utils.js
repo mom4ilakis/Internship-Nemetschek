58da70f6597e730b5beac6e7b1d96ae484033e34
Momchil Marinov <mom4ilakis@gmail.com>
2020-02-06 16:02:37 +0200

    Added default values for wind and precipitation, fixed export

--- Web/WeatherAppPart2/src/utils.js
+++ Web/WeatherAppPart2/src/utils.js
@@ -1,23 +1,28 @@
-export function formatData (response) {
+const formatData = (response) => {
     const data = {};
     // temperature info
-    data.minTemp = response.main.temp_min;
-    data.maxTemp = response.main.temp_max;
-    data.currentTemp = response.main.temp;
-    data.feelsLikeTemperature = response.main.feels_like;
+    data.minTemp = response.data.main.temp_min || null;
+    data.maxTemp = response.data.main.temp_max || null;
+    data.currentTemp = response.data.main.temp || null;
+    data.feelsLikeTemperature = response.data.main.feels_like || null;
     // quick info i.e  synny/stormy etc
-    data.description = response.weather[0].description;
-    data.condition = response.weather.main;
-    data.clouds = response.clouds.all;
+    data.description = response.data.weather[0].description || null;
+    data.condition = response.data.weather.main || null;
+    data.clouds = response.data.clouds.all || null;
     // wind ifno
-    data.wind.speed = response.wind.speed;
-    data.wind.direction = response.wind.deg;
+    data.wind = {};
+    data.wind.speed = (response.data.wind && response.data.wind.speed) || null;
+    data.wind.direction = (response.data.wind && response.data.wind.deg) || null;
     // precipitation info
-    data.humidity = response.main.humidity;
-    data.precipitation.snow = response.snow['3h'];
-    data.precipitation.rain = response.rain['3h'];
+    data.humidity = response.data.main.humidity || null;
+    data.precipitation = {};
+    data.precipitation.snow = (response.data.snow && response.data.snow['3h']) || null;
+    data.precipitation.rain = (response.data.rain && response.data.rain['3h']) || null;
     // other info
-    data.sunrise = response.sys.sunrise;
-    data.sunset = response.sys.sunset;
+    data.sunrise = response.data.sys.sunrise || null;
+    data.sunset = response.data.sys.sunset || null;
     return data;
-}
+};
+export {
+    formatData
+};
