959bc63451e74581365d740bd79d03210e5cc7fa
Momchil Marinov <mom4ilakis@gmail.com>
2020-02-06 16:01:24 +0200

    Removed caching

--- Web/WeatherAppPart2/src/api.js
+++ Web/WeatherAppPart2/src/api.js
@@ -1,17 +1,27 @@
 import axios from 'axios';
-const api = {
-    location,
-    response,
-    get: (location, url, API_key) => {
-        if (api.location === location) {
-            return api.response;
-        }
-        api.location = location;
-        const params = { q: location, APPID: API_KEY };
-        api.response = axios.get(url, { params: params });
-        return api.response;
-    }
+// const api = {
+//     location: null,
+//     promise: null,
+//     get: (location, url, API_KEY) => {
+//         if (api.location === location) {
+//             return api.promise;
+//         }
+//         api.location = location;
+//         const params = { q: location, APPID: API_KEY };
+//         api.promise = axios.get(url, { params: params });
+//         return api.promise;
+//     }
 
+// };
+
+const API_KEY = 'b91abb81348473629b33669fb955de64';
+const url = 'http://api.openweathermap.org/data/2.5/weather';
+
+const api = {
+    get: (location) => {
+        const params = { q: location, APPID: API_KEY };
+        return axios.get(url, { params: params });
+    }
 };
 
-export default Api;
+export default api;
