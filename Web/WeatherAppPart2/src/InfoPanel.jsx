aec3a909633234bf8c51eefb221ccb2814826b38
Momchil Marinov <mom4ilakis@gmail.com>
2020-02-06 11:26:04 +0200

    Initial commit

---
+++ Web/WeatherAppPart2/src/InfoPanel.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import TemperatureInfo from './TemperatureInfo'
+import QuickInfo from './QuckInfo'
+import WindInfo from './WindInfo'
+import Precpitation from './Precipitation'
+
+class InfoPanel extends React.Component {
+    render () {
+        return (
+            <div>
+                <TemperatureInfo/>
+                <QuickInfo/>;
+                <WindInfo/>;
+                <Precpitation/>
+            </div>
+        );
+    }
+}
+export default InfoPanel;
