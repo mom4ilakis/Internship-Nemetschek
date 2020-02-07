671a0721f6ff4b42f067358f477a5bd2debd33f5
Momchil Marinov <mom4ilakis@gmail.com>
2020-02-07 11:13:35 +0200

    Changed to not render if no data is available; Added line-breaks

--- Web/WeatherAppPart2/src/Precipitation.jsx
+++ Web/WeatherAppPart2/src/Precipitation.jsx
@@ -5,9 +5,9 @@ class Precipitation extends React.Component {
     render () {
         return (
             <div>
-                <p>Humidity is {this.props.data.humidity || 0} %</p>
-                <p>Rain: {this.props.data.rain || 'None'} </p>
-                <p>Snow: {this.props.data.snow || 'None'}</p>
+                {this.props.data.snow && <div>Snow : {this.props.data.snow}<br/></div>}
+                {this.props.data.rain && <div>Rain: {this.props.data.rain }<br/></div>}
+                {(this.props.data.rain || this.props.data.snow) && <hr/>}
             </div>
         );
     }
