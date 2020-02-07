483bac1c2be7b286385350b07e79ff349a4c5d04
Momchil Marinov <mom4ilakis@gmail.com>
2020-02-07 11:11:53 +0200

    Made updates to be real-time

--- Web/WeatherAppPart2/src/App.jsx
+++ Web/WeatherAppPart2/src/App.jsx
@@ -3,48 +3,45 @@ import api from './api';
 import InfoPanel from './InfoPanel';
 import { formatData } from './utils.js';
 
+// one minute is 60000 ms
+const interval = 300000;
+
 class App extends React.Component {
     constructor (props) {
         super(props);
         this.state = {
-            searched: false,
+            location: 'Sofia',
             data: { wind: {}, precipation: {} }
         };
     }
 
     componentDidMount () {
-
+        this.loadData();
+        this.timerID = setInterval(this.loadData, interval);
     }
 
-    loadData = (location = 'Sofia') => {
-        api.get(location)
+    componentWillUnmount () {
+        this.timerID && clearInterval(this.timerID);
+    }
+
+    loadData = () => {
+        api.get(this.state.location)
             .then(res => this.setState({ data: formatData(res) }))
             .catch(err => console.error(err));
     }
 
     handleSearchForLocation = event => {
         if (event.which === 13) {
-            this.loadData(event.target.value);
-            this.setState({ searched: true });
-        }
-    }
-
-    info () {
-        if (this.state.data === null) {
-            return <p/>;
-        } else {
-            return <InfoPanel data={this.state.data}/>;
+            this.setState({ location: event.target.value }, this.loadData);
         }
     }
 
     render () {
         return (
             <div>
-                <input type='text' id='location' onKeyPress={this.handleSearchForLocation}/>
-                {
-                    this.state.searched &&
-                    <InfoPanel data={this.state.data}/>
-                }
+                <input type='text' onKeyPress={this.handleSearchForLocation} defaultValue={this.state.location}/>
+                <hr/>
+                <InfoPanel data={this.state.data}/>
             </div>
         );
     }
