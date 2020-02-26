29146e978ecdf4add2ec52d15139f74404f53b9e
Momchil Marinov <mom4ilakis@gmail.com>
2020-02-26 14:40:26 +0200

    Made author to call AuthorSerializer

--- Web/Blog/blog/post/serializers.py
+++ Web/Blog/blog/post/serializers.py
@@ -1,9 +1,14 @@
 from .models import Post
 from rest_framework import serializers
 
+from author.serializers import AuthorSerializer
+
 
 class PostSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(read_only=True, required=False)
+
     class Meta:
         model = Post
         fields = ['title', 'content', 'author', 'cover', 'date']
         read_only_fields = ['date', 'author']
+
