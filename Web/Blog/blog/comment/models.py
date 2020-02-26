48ce52ca46a65281177cca67a1c981d39026846b
Momchil Marinov <mom4ilakis@gmail.com>
2020-02-26 14:38:32 +0200

    Changed date from models.DateField to models.DateTimeField

--- Web/Blog/blog/comment/models.py
+++ Web/Blog/blog/comment/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from  datetime import date
+from  datetime import datetime
 
 from author.models import Author
 from post.models import Post
@@ -9,7 +9,7 @@ from post.models import Post
 class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.CharField(max_length=300)
-    date = models.DateField(default=date.today())
+    date = models.DateTimeField(default=datetime.now)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
     def __str__(self):
