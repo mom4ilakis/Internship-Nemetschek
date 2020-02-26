2af8fa36fbdfed59bf11eb99e781314321cf9814
Momchil Marinov <mom4ilakis@gmail.com>
2020-02-26 14:39:01 +0200

    Made author to call UserSerializer

--- Web/Blog/blog/comment/serializers.py
+++ Web/Blog/blog/comment/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 from .models import Comment
 
+from author.serializers import UserSerializer
+
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ['author', 'content', 'date', 'post']
         read_only_fields = ['date', 'author']
         model = Comment
+
+    author = UserSerializer(read_only=True, required=False)
