c5b098003fb1a3c11f39f83449ebe821aee5937d
Momchil Marinov <mom4ilakis@gmail.com>
2020-02-26 14:40:46 +0200

    Moved fixtures to conftest.py

--- Web/Blog/blog/post/tests.py
+++ Web/Blog/blog/post/tests.py
@@ -1,34 +1,6 @@
 import pytest
-from datetime import date
-from rest_framework.test import APIClient
+from datetime import datetime
 
-from post.models import Post
-from author.models import Author
-
-
-@pytest.fixture
-def anon_client():
-    return APIClient()
-
-
-@pytest.fixture
-def author():
-    author = Author(username='test_user', password='123456', avatar='none')
-    author.save()
-    return author
-
-
-@pytest.fixture
-def client(author):
-    client = APIClient()
-    client.force_authenticate(user=author)
-    return client
-
-
-@pytest.fixture
-def post(author):
-    return Post.objects.create(title="Title", content="Content", cover="cover",
-                               author=author, date=date.today())
 
 
 @pytest.mark.django_db
@@ -38,32 +10,39 @@ def test_no_posts(anon_client):
 
 
 @pytest.mark.django_db
-def test_create_post(client, post):
+def test_create_post(client, author):
     result = client.post('/posts/', {
         'title': 'title',
         'content': 'content',
         'cover': 'cvr'
-    },
-        format='json'
-    )
+    })
 
     assert result.status_code == 201
-    # assert result.data
+    del result.data['date']
+    assert result.data == {
+        'title': 'title',
+        'content': 'content',
+        'cover': 'cvr',
+        'author': {
+            'username': author.username,
+            'email': author.email,
+            'avatar': author.avatar
+        },
+    }
 
 
 @pytest.mark.django_db
 def test_create_post_invalid_user(anon_client):
-    result = anon_client.post('/posts/',
-                              {
-                                "title": "wrong title",
-                                "content": "free",
-                                "cover": "cvr"
-                              })
+    result = anon_client.post('/posts/', {
+        "title": "wrong title",
+        "content": "free",
+        "cover": "cvr"
+    })
     assert result.status_code == 401
 
 
 @pytest.mark.django_db
-def test_get_listposts(client):
+def test_list_posts(client):
     result = client.get('/posts/')
 
     assert result.status_code == 200
@@ -73,12 +52,14 @@ def test_get_listposts(client):
 def test_get_existing_post(client, post):
     result = client.get(f'/posts/{post.pk}/')
     assert result.status_code == 200
+    del result.data['date']
     assert result.data == {
         'title': post.title,
         'content': post.content,
         'cover': post.cover,
-        'date': str(post.date),
-        'author': post.author.pk
+        'author': {'username': post.author.username,
+                   'email': post.author.email,
+                   'avatar': post.author.avatar}
     }
 
 
@@ -101,7 +82,7 @@ def test_delete_non_existing_post(client):
 
 
 @pytest.mark.django_db
-def test_get_post_non_auth_user(anon_client, client, post):
+def test_get_post_non_auth_user(anon_client, post):
     result = anon_client.get(f'/posts/{post.pk}/')
     assert result.status_code == 200
     result.data == {
@@ -119,3 +100,31 @@ def test_update_non_auth_user(anon_client, client, post):
         "title": "new title"
     })
     assert result.status_code == 401
+
+
+@pytest.mark.django_db
+def test_update_non_author_user(user_client, user, post):
+    result = user_client.patch(f'/posts/{post.pk}/', {
+        'title': 'vulgar title'
+    })
+    assert result.status_code == 403
+
+
+@pytest.mark.django_db
+def test_update_author_own_post(client, post):
+    result = client.patch(f'/posts/{post.pk}/', {
+        'title': 'new shiny title'
+    })
+    assert result.status_code == 200
+    assert result.data['title'] == 'new shiny title'
+
+# тест за това регистриран потребител може ли да редактира пост done
+# тест за това _автора_ на поста може ли да редактира собствения си пост done
+
+
+@pytest.mark.django_db
+def test_update_author_not_own_post(post, another_client):
+    result = another_client.patch(f'/posts/{post.pk}/', {
+        'title': 'title-less ;)'
+    })
+    assert result.status_code == 403
