d5a7cfea4a9eb793326f914f0ff388fdaafa690d
Momchil Marinov <mom4ilakis@gmail.com>
2020-03-04 14:18:05 +0200

    Added serializer for postPreview

--- Web/Blog/blog/post/serializers.py
+++ Web/Blog/blog/post/serializers.py
@@ -1,7 +1,7 @@
 from .models import Post
 from rest_framework import serializers
 
-from author.serializers import AuthorSerializer
+from author.serializers import AuthorSerializer, UsernameSerializer
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -11,3 +11,12 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = ['title', 'content', 'author', 'cover', 'date']
         read_only_fields = ['date', 'author']
+
+
+class PostPreviewSerializer(serializers.ModelSerializer):
+    author = UsernameSerializer(read_only=True, required=False)
+
+    class Meta:
+        model = Post
+        fields = ['title', 'author', 'cover', 'date']
+        read_only_fields = ['title', 'author', 'cover', 'date']
