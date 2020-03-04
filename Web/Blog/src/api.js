11df4f3e5fed1074e8550d0c045c9052cce8e482
Momchil Marinov <mom4ilakis@gmail.com>
2020-03-04 16:58:54 +0200

    FIxed return statements

--- Web/Blog/src/api.js
+++ Web/Blog/src/api.js
@@ -8,16 +8,16 @@ const request = axios.create({
 
 const api = {
     get: (url, auth_token) => {
-        request.get(url, { Authorisation: `Token : ${auth_token}` });
+        return request.get(url, { Authorisation: `Token : ${auth_token}` });
     },
     post: (url, data, auth_token) => {
-        request.patch(url, data, { Authorisation: `Token : ${auth_token}` });
+        return request.patch(url, data, { Authorisation: `Token : ${auth_token}` });
     },
     patch: (url, new_data, auth_token) => {
-        request.patch(url, new_data, { Authorisation: `Token : ${auth_token}` });
+        return request.patch(url, new_data, { Authorisation: `Token : ${auth_token}` });
     },
     delete: (url, user, auth_token) => {
-        request.delete(url, { Authorisation: `Token : ${auth_token}` });
+        return request.delete(url, { Authorisation: `Token : ${auth_token}` });
     },
     login: (name, pass) => {
         const response = request.post('/auth/', {
