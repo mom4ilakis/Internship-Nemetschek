d876cb6c09d1bb5dcd64b58739ceed659bcf0a52
Momchil Marinov <mom4ilakis@gmail.com>
2020-03-04 14:18:51 +0200

    Added url for posts previews

--- Web/Blog/blog/post/urls.py
+++ Web/Blog/blog/post/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import SimpleRouter
-from .views import PostViewSet
+from .views import PostViewSet, PostPreviewSet
 
 router = SimpleRouter()
 router.register('posts', PostViewSet)
+router.register('posts_preview', PostPreviewSet)
 
 urlpatterns = router.urls
