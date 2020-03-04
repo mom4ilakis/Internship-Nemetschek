37d84eac42cab8229e0374b53ee7ec332fb63e50
Momchil Marinov <mom4ilakis@gmail.com>
2020-03-04 16:59:10 +0200

    Added render logic

--- Web/Blog/src/app.jsx
+++ Web/Blog/src/app.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import Post from './Post';
+import HomePage from './PostsPreview';
+import Login from './Login';
+import api from './api';
+import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
 
 class App extends React.Component {
-    constructor(props) {
+    constructor (props) {
         super(props);
         this.state = {
-            user_token: null,
+            user_token: null
         };
     }
+
+    render () {
+        return (
+            <Router>
+                <Link to='/authors/'>Authors</Link>
+                <br/>
+                <Link to='/login/'>log in</Link>
+                <Switch>
+                    <Route exact path='/'><HomePage/></Route>
+                    <Route exact path='/authors/'>Authors</Route>
+                    <Route exact path='/login/'><Login/></Route>
+                </Switch>
+            </Router>
+        );
+    }
 }
+
+export default App;
