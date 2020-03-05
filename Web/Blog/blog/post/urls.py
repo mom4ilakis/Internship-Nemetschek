8e56719e80bcb6887120644b45b26d7f62ad7c11
Momchil Marinov <mom4ilakis@gmail.com>
2020-03-05 14:52:37 +0200

    Added router path for CommentOnPostViewSet

--- Web/Blog/blog/post/urls.py
+++ Web/Blog/blog/post/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import SimpleRouter
-from .views import PostViewSet, PostPreviewSet
+from .views import PostViewSet, PostPreviewSet, CommentsOnPostViewSet
 
 router = SimpleRouter()
 router.register('posts', PostViewSet)
 router.register('posts_preview', PostPreviewSet)
+router.register('comments_on_post', CommentsOnPostViewSet)
 
 urlpatterns = router.urls
