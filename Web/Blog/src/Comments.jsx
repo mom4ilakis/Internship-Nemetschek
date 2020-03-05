bae35f6c7bf693ed21eb01e505e274d54c217909
Momchil Marinov <mom4ilakis@gmail.com>
2020-03-05 16:43:12 +0200

    Init commit

--- Web/Blog/src/Comments.jsx
+++ Web/Blog/src/Comments.jsx
@@ -2,17 +2,41 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Replies from './Replies';
 
+import api from './api';
+import utils from './utils';
+
 class Comments extends React.Component {
+    state = {
+        data: []
+    }
 
     componentDidMount () {
+        const postID = this.props.postID;
+        api.get(`/comments_on_post/${postID}/`)
+            .then(({ data }) => {
+                console.log(data);
+                this.setState({ data: data });
+            })
+            .catch(err => console.log(err));
     }
 
     render () {
         return (
             <React.Fragment>
-                <div>
-                </div>
-                <Replies replies={this.props.comment.replies}/>
+                {
+                    this.state.data.map(comment =>
+                        <div key={`comment_ ${comment.pk}`}>
+                            <p>
+                                {comment.content}
+                            </p>
+                            {comment.author}
+                            <br/>;
+                            {utils.formatDate(comment.date)}
+                            {utils.formatTime(comment.date)}
+                        </div>
+                    )
+                }
+
             </React.Fragment>
         );
     }
@@ -21,4 +45,4 @@ Comment.propTypes = {
     comments: PropTypes.array
 };
 
-export default Comment;
+export default Comments;
