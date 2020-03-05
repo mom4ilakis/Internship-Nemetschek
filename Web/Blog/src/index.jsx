ec3faee472168b878c014bfa7776f000f734d00a
Momchil Marinov <mom4ilakis@gmail.com>
2020-03-05 14:55:38 +0200

    Fixed indenting

--- Web/Blog/src/index.jsx
+++ Web/Blog/src/index.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './css/index-styles.scss';
-import Login from './login';
+import Login from './Login';
+import App from './App';
+
 ReactDOM.render(
     <div>
         <p>
             <h1>Hello world</h1>
         </p>
-        <Login/>
+        <App/>
     </div>
     ,
     document.getElementById('root')
