c543d797c7b950c8ec78fcafac1371773400aaff
Momchil Marinov <mom4ilakis@gmail.com>
2020-03-06 15:48:27 +0200

    Added border to div tag, added new line tags

--- Web/Blog/src/replies.jsx
+++ Web/Blog/src/replies.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import utils from './utils';
+
 class Replies extends React.Component {
     render () {
         return (
             <div>
                 {
                     this.props.replies.map(reply =>
-                        <div key={reply.id}>
-                            {reply.author}
+                        <div style={{ borderStyle: 'solid' }} key={reply.id}>
                             {reply.content}
+                            <br/>
+                            {reply.author.username}
+                            <br/>
                             {reply.date}
                         </div>
                     )
