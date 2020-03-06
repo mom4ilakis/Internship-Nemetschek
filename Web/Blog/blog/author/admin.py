64fbad4b68fc779a59b6e73baa5eaf77c2d41dc5
Momchil Marinov <mom4ilakis@gmail.com>
2020-03-06 15:44:17 +0200

    Added custom admin class

--- Web/Blog/blog/author/admin.py
+++ Web/Blog/blog/author/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Author
 
 
-admin.site.register(Author)
+@admin.register(Author)
+class AuthorAdmin(UserAdmin):
+    model = Author
+    list_display = ['id', 'username', 'email', 'first_name', 'last_name', 'avatar']
+    readonly_fields = ['id']
+    ordering = ['-id']
