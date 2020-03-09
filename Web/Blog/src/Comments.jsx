1cedaf162c3dc8e959d9bc8dbfdf33a0cffe9aa5
Momchil Marinov <mom4ilakis@gmail.com>
2020-03-09 17:35:54 +0200

    Added commenting functionality

--- Web/Blog/src/Comments.jsx
+++ Web/Blog/src/Comments.jsx
@@ -6,24 +6,37 @@ import api from './api';
 
 class Comments extends React.Component {
     state = {
-        data: []
+        data: [],
+        commentBox: null
     }
 
     componentDidMount () {
         const postID = this.props.postID;
         api.get(`/comments_on_post/${postID}/`)
             .then(({ data }) => {
-                this.setState({ data: data });
+                const commentBox = document.getElementById(`commentBox-${postID}`);
+                this.setState({ data: data, commentBox: commentBox });
             })
             .catch(err => console.log(err));
+    }
+
+    handleCommentSubmit = event => {
+        api.post('/comments/', { content: this.state.commentBox.value, post: this.props.postID })
+            .then(({ data }) => {
+                const newData = [...this.state.data, data];
+                this.setState({ data: newData });
+                this.state.commentBox.value = '';
+            });
     }
 
     render () {
         return (
             <React.Fragment>
+                {this.props.logged && <input type='text' id={`commentBox-${this.props.postID}`} />}
+                {this.props.logged && <button name='commentButton' onClick={this.handleCommentSubmit}>Comment</button>}
                 {this.state.data.map(comment =>
-                    <React.Fragment key={comment.pk}>
-                        <Comment pk={comment.pk} content={comment.content} author={comment.author} date={comment.date} replies={comment.replies} />
+                    <React.Fragment key={comment.id}>
+                        <Comment comment={comment} logged={this.props.logged}/>
                         <br/>
                     </React.Fragment>
                 )}
@@ -32,7 +45,8 @@ class Comments extends React.Component {
     }
 }
 Comments.propTypes = {
-    postID: PropTypes.string
+    postID: PropTypes.string,
+    logged: PropTypes.bool
 };
 
 export default Comments;
