753541b8c3c64b0278fcab41bb874ed5106c9e8f
Momchil Marinov <mom4ilakis@gmail.com>
2020-03-09 17:34:33 +0200

    Added Make reply functionality

--- Web/Blog/src/Comment.jsx
+++ Web/Blog/src/Comment.jsx
@@ -1,31 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import api from './api';
 import utils from './utils';
 import Replies from './Replies';
 
 class Comment extends React.Component {
+    state = {
+        content: null,
+        replyBox: null,
+        replies: []
+    }
+
+    componentDidMount () {
+        const replyBox = document.getElementById(`replyBox-${this.props.comment.id}`);
+        this.setState({ replies: this.props.comment.replies, replyBox: replyBox });
+    }
+
+    handleMakeReply = (event) => {
+        api.post('/replies/', { content: this.state.content, comment: this.props.comment.id })
+            .then(response => {
+                const newReplies = [...this.state.replies, response.data];
+                this.state.replyBox.value = '';
+                this.setState({ replies: newReplies, content: null });
+            });
+    }
+
+    handleTyping = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
     render () {
+        const { comment } = this.props;
+        const { id, date, author, content } = comment;
+
         return (
-            <div style={{ borderStyle: 'solid' }} key={`comment_ ${this.props.pk}`}>
+            <div className='comment' key={`comment-${id}`}>
                 <p>
-                    {this.props.content}
+                    {content}
                 </p>
-                {this.props.author.username}
+                <div className='author'>{author.username}</div>
+                <div className='posted'>{`${utils.formatDate(date)} ${utils.formatTime(date)}`}</div>
                 <br/>
-                {utils.formatDate(this.props.date)}
+                {this.props.logged && <input type='text' id={`replyBox-${comment.id}`} name='content' onChange={ this.handleTyping }/>}
+                {this.props.logged && <button onClick={this.handleMakeReply}>Reply</button>}
                 <br/>
-                {utils.formatTime(this.props.date)}
-                <Replies replies={this.props.replies}/>
+                <Replies replies={this.state.replies}/>
             </div>);
     }
 }
 
 Comment.propTypes = {
-    pk: PropTypes.string,
-    content: PropTypes.string,
-    author: PropTypes.object,
-    date: PropTypes.string,
-    replies: PropTypes.object
+    comment: PropTypes.object,
+    logged: PropTypes.bool
 };
 export default Comment;
