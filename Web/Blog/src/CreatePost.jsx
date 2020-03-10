7f1c5e0b37fdfcd2dcde28041f861cd519b32bb8
Momchil Marinov <mom4ilakis@gmail.com>
2020-03-10 18:13:22 +0200

    Initt commit

--- Web/Blog/src/CreatePost.jsx
+++ Web/Blog/src/CreatePost.jsx
@@ -1,20 +1,58 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import api from './api';
+import { Redirect, withRouter } from 'react-router';
 
 class CreatePost extends React.Component {
+    state = {
+        title: null,
+        content: null,
+        cover: null
+    }
+
+    handleCancel = () => {
+        this.props.history.push('/');
+    }
+
+    handlePost = () => {
+        console.log(this.state);
+        api.post('/posts/', { title: this.state.title, content: this.state.content, cover: this.state.cover });
+        this.props.history.push('/');
+    }
+
+    handleChange = (event) => {
+        console.log(event.target);
+        console.log(event.target.value);
+
+        this.setState({ [event.target.id]: event.target.value });
+    }
+
     render () {
         return (
             <React.Fragment>
-                Title:
-                <input type='text' id='title'/>
-                Content:
-                <input type='textarea' id='content'/>
-                Cover:
-                <input type='textarea' id='cover'/>
-                <button>Post</button>
-                <button>Cancel</button>
+                { this.props.isAuthor &&
+                    <div>
+                        Title:
+                        <input type='text' id='title' onChange={this.handleChange}/>
+                        Content:
+                        <textarea id='content' onChange={this.handleChange}/>
+                        Cover:
+                        <input type='text' id='cover' onChange={this.handleChange}/>
+                        <button onClick={this.handlePost}>Post</button>
+                        <button onClick={this.handleCancel}>Cancel</button>
+                    </div>}
+                {!this.props.isAuthor &&
+                    <div>
+                        You are not an author !
+                    </div>}
             </React.Fragment>
         );
     }
 }
 
-export default CreatePost;
+CreatePost.propTypes = {
+    isAuthor: PropTypes.bool
+}
+
+export default withRouter(CreatePost);
