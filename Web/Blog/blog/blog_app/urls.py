aef18944a9ee76bb8883aba3d24f7639ae3c83ec
Momchil Marinov <mom4ilakis@gmail.com>
2020-03-10 18:11:06 +0200

    Imported CustomAuthToken

--- Web/Blog/blog/blog_app/urls.py
+++ Web/Blog/blog/blog_app/urls.py
@@ -4,12 +4,12 @@ from rest_framework.authtoken.views import obtain_auth_token
 from author.urls import urlpatterns as author_urls
 from comment.urls import urlpatterns as comment_urls
 from post.urls import urlpatterns as post_urls
-
+from .authentication import CustomAuthToken
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('auth/', obtain_auth_token, name='api_token_auth'),
+    path('auth/', CustomAuthToken.as_view(), name='api_token_auth'),
     path('', include(author_urls)),
     path('', include(comment_urls)),
     path('', include(post_urls))
