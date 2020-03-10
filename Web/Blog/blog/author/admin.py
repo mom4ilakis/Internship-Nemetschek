bfef9ba30542e9a692c687067a6f8527bbe0bac9
Momchil Marinov <mom4ilakis@gmail.com>
2020-03-10 18:06:48 +0200

    Removed favatar, irst,  and last name  from list_view, added is author to fields

--- Web/Blog/blog/author/admin.py
+++ Web/Blog/blog/author/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Author
+from .models import User
 
 
-@admin.register(Author)
-class AuthorAdmin(UserAdmin):
-    model = Author
-    list_display = ['id', 'username', 'email', 'first_name', 'last_name', 'avatar']
+@admin.register(User)
+class AuthorAdmin(admin.ModelAdmin):
+    model = User
+    list_display = ['id', 'username', 'email', 'is_author']
+    fields = ['id', 'username', 'email', 'first_name', 'last_name', 'avatar', 'is_author']
     readonly_fields = ['id']
     ordering = ['-id']
