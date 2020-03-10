16f36e762a237a0a31444918f6736506586837a7
Momchil Marinov <mom4ilakis@gmail.com>
2020-03-10 18:12:53 +0200

    Added isAuthor to state of component

--- Web/Blog/src/app.jsx
+++ Web/Blog/src/app.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Post from './Post';
 import HomePage from './PostsPreview';
 import Login from './Login';
+import CreatePost from './CreatePost';
+
 import api from './api';
 
 import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
@@ -10,13 +12,13 @@ class App extends React.Component {
     constructor (props) {
         super(props);
         this.state = {
-            logged: false
+            logged: false,
+            isAuthor: false
         };
     }
 
-    userLoggedIn = () => {
-        console.log('Logged in.');
-        this.setState({ logged: true });
+    userLoggedIn = (isAuthor) => {
+        this.setState({ logged: true, isAuthor: isAuthor });
     }
 
     userLoggedOut = () => {
@@ -26,12 +28,15 @@ class App extends React.Component {
     render () {
         return (
             <Router>
+                {this.state.isAuthor && <Link to='/create-post/'>New Post</Link>}
+                <br/>
                 <Link to='/authors/'>Authors</Link>
                 <br/>
                 <Link to='/'>Home</Link>
                 <br/>
                 <Link to='/login/'>log in</Link>
                 <Switch>
+                    <Route exact path='/create-post/'><CreatePost isAuthor={this.state.isAuthor}/></Route>
                     <Route exact path='/'><HomePage/></Route>
                     <Route exact path='/authors/'>Authors</Route>
                     <Route exact path='/login/'><Login callback={this.userLoggedIn}/></Route>
