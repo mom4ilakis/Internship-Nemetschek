b10481c2ddb3b4a27daf96bc9c58428cd9c623cd
Momchil Marinov <mom4ilakis@gmail.com>
2020-03-10 18:11:46 +0200

    Changed Author to User

--- Web/Blog/blog/post/models.py
+++ Web/Blog/blog/post/models.py
@@ -1,10 +1,12 @@
 from django.db import models
-from author.models import Author
 from datetime import datetime
+
+from author.models import User
 
 
 class Post(models.Model):
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, )
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE, )
     title = models.CharField(max_length=160)
     cover = models.CharField(max_length=200)
     content = models.TextField(max_length=1000)
