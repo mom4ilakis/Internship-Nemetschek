70e99194030a54f8519af0352d88b5b404fbf7a2
Momchil Marinov <mom4ilakis@gmail.com>
2020-03-10 18:07:32 +0200

    Removed AuthorSerializer

--- Web/Blog/blog/author/serializers.py
+++ Web/Blog/blog/author/serializers.py
@@ -1,19 +1,12 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
 
-from .models import Author
+from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name']
-
-
-class AuthorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Author
-        fields = ['username', 'email', 'avatar', 'first_name', 'last_name']
+        fields = ['id', 'username', 'email', 'avatar', 'first_name', 'last_name', 'is_author']
 
 
 class UsernameSerializer(serializers.ModelSerializer):
