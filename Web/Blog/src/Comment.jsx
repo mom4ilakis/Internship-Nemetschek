8b6d4b804cf788ca0efb3897208456346de6f380
Momchil Marinov <mom4ilakis@gmail.com>
2020-03-11 17:46:28 +0200

    Added logic for editing comment

--- Web/Blog/src/Comment.jsx
+++ Web/Blog/src/Comment.jsx
@@ -4,12 +4,21 @@ import PropTypes from 'prop-types';
 import api from './api';
 import utils from './utils';
 import Replies from './Replies';
+import EditComment from './EditComment';
 
 class Comment extends React.Component {
     state = {
         content: null,
+        editing: false,
         replyBox: null,
         replies: []
+    }
+
+    toggleEdit = (newContent) => {
+        this.setState({ editing: !this.state.editing });
+        if (!newContent) {
+            this.setState({ content: newContent });
+        }
     }
 
     componentDidMount () {
@@ -35,23 +44,31 @@ class Comment extends React.Component {
         const { id, date, author, content } = comment;
 
         return (
-            <div className='comment' key={`comment-${id}`}>
+            (this.state.editing && <EditComment id={id} callback={this.toggleEdit} content={this.state.content}/>) ||
+            <div className='box' key={`comment-${id}`}>
+
                 <p>
                     {content}
                 </p>
                 <div className='author'>{author.username}</div>
                 <div className='posted'>{`${utils.formatDate(date)} ${utils.formatTime(date)}`}</div>
                 <br/>
-                {this.props.logged && <input type='text' id={`replyBox-${comment.id}`} name='content' onChange={ this.handleTyping }/>}
-                {this.props.logged && <button onClick={this.handleMakeReply}>Reply</button>}
+                <div className='buttons has-addons are-small'>
+                    {author.id === this.props.userID && <button onClick={this.handleDeleteComment} className='button is-danger'>Delete</button>}
+                    {author.id === this.props.userID && <button onClick={this.toggleEdit} className='button is-dark'>Edit</button>}
+                </div>
+
+                {this.props.logged && <input type='text' className='input' id={`replyBox-${comment.id}`} name='content' onChange={ this.handleTyping }/>}
+                {this.props.logged && <button className='button is-normal is-primary' onClick={this.handleMakeReply}>Reply</button>}
                 <br/>
-                <Replies replies={this.state.replies}/>
+                <Replies replies={this.state.replies} userID={this.props.userID}/>
             </div>);
     }
 }
 
 Comment.propTypes = {
     comment: PropTypes.object,
-    logged: PropTypes.bool
+    logged: PropTypes.bool,
+    userID: PropTypes.number
 };
 export default Comment;
