d980e6b25b3e290ad2fdd6ec51e7b4c6e8f699aa
Momchil Marinov <mom4ilakis@gmail.com>
2020-03-11 17:49:23 +0200

    Post now holds author, title, content etc, not a as a data

--- Web/Blog/src/post.jsx
+++ Web/Blog/src/post.jsx
@@ -1,38 +1,93 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Comments from './Comments';
+import { Link } from 'react-router-dom';
 
+import Comments from './Comments';
 import api from './api';
 import utils from './utils';
+import EditPost from './EditPost';
 
 class Post extends React.Component {
     state = {
-        data: {}
+        author: {},
+        title: null,
+        content: null,
+        date: null,
+        editable: false
+    }
+
+    loadData = (passedData) => {
+        const postID = this.props.match.params.postID;
+        console.log(`passed data is ${passedData}`);
+        if (!passedData) {
+            api.get(`/posts/${postID}/`)
+                .then(({ data }) => {
+                    this.setState({
+                        author: data.author,
+                        title: data.title,
+                        content: data.content,
+                        cover: data.cover,
+                        date: data.date
+                    });
+                })
+                .catch(err => console.log(err));
+        } else {
+            this.setState({
+                title: passedData.title,
+                content: passedData.content,
+                cover: passedData.cover
+            });
+        }
     }
 
     componentDidMount () {
+        this.loadData();
+    }
+
+    handleDeletePost = () => {
         const postID = this.props.match.params.postID;
-        api.get(`/posts/${postID}/`)
-            .then(({ data }) => {
-                this.setState({ data: data });
-            })
-            .catch(err => console.log(err));
+        api.delete(`/posts/${postID}/`).then(() => {
+            this.props.history.push('/');
+        });
+    }
+
+    toggleEditing = (newData) => {
+        this.loadData(newData);
+        this.setState({ editable: !this.state.editable });
+    }
+
+    handleClick = () => {
+        this.setState({ editable: true });
     }
 
     render () {
         return (
             <React.Fragment>
-                <div style={{ borderStyle: 'solid' }}>
-                    <h3>{this.state.data.title}</h3>
-                    <img src={this.state.data.cover}/>
-                    <p>{this.state.data.content}</p>
-                    {utils.formatDate(this.state.data.date)}
-                    <br/>
+                {(this.state.editable &&
+                <EditPost
+                    title={this.state.title}
+                    content={this.state.content}
+                    cover={this.state.cover}
+                    callback={this.toggleEditing}
+                />) ||
+                    <div className='box'>
+                        <div className='content'>
+                            <h3>{this.state.title}</h3>
+                            <img src={this.state.cover}/>
+                            <p>{this.state.content}</p>
+                            {utils.formatDate(this.state.date)}
+                            <br/>
                     Time:
-                    {utils.formatTime(this.state.data.date)}
-                    <Comments postID={this.props.match.params.postID} token={this.state.token} logged={this.props.logged}/>
-                    <br/>
-                </div>
+                            {utils.formatTime(this.state.date)}
+                        </div>
+                        <div className='buttons has-addons are-small'>
+                            {this.state.author.id === this.props.userID && <button className='button is-danger' onClick={this.handleDeletePost}>Delete</button> }
+                            {this.state.author.id === this.props.userID && <button className='button is-dark' to='/edit_post/' onClick={this.handleClick}>Edit</button>}
+                        </div>
+                        <Comments postID={this.props.match.params.postID} logged={this.props.logged} userID={this.props.userID}/>
+                        <br/>
+                    </div>
+                }
             </React.Fragment>
 
         );
@@ -41,6 +96,7 @@ class Post extends React.Component {
 
 Post.propTypes = {
     match: PropTypes.object,
-    logged: PropTypes.bool
+    logged: PropTypes.bool,
+    userID: PropTypes.number
 };
 export default Post;
