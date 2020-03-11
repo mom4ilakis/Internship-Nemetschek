48fd4e0014cd93d5ce9526f34fee6dda8c25a76c
Momchil Marinov <mom4ilakis@gmail.com>
2020-03-11 17:47:48 +0200

    Init Commit

---
+++ Web/Blog/src/EditPost.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router';
+import api from './api';
+
+class EditPost extends React.Component {
+    state = {
+        title: this.props.title,
+        content: this.props.content,
+        cover: this.props.cover
+    }
+
+    componentDidMount () {
+        const ids = ['title', 'content', 'cover'];
+        ids.forEach(id => {
+            document.getElementById(id).value = this.props[id];
+        });
+    }
+
+    handleSubmit = () => {
+        const postID = this.props.match.params.postID;
+        api.patch(`/posts/${postID}/`, this.state);
+        this.props.callback(this.state);
+        this.props.history.push(`/posts/${postID}/`);
+    }
+
+    handleCancel = () => {
+        const postID = this.props.match.params.postID;
+        this.props.callback();
+        this.props.history.push(`/posts/${postID}/`);
+    }
+
+    handleDelete = () => {
+        const postID = this.props.match.params.postID;
+        api.delete(`/posts/${postID}/`).then(() => {
+            this.props.history.push('/');
+        });
+    }
+
+    handleChange = (event) => {
+        this.setState({ [event.target.id]: event.target.value });
+    }
+
+    render () {
+        return (
+            <div className='box'>
+
+                <label className='label'>New Title</label>
+                <input id='title' type='text' className='input' onChange={this.handleChange}/>
+
+                <label className='label'>New Content</label>
+                <textarea id='content' className='input' onChange={this.handleChange}/>
+
+                <label className='label'>New Cover</label>
+                <input id='cover' type='text' className='input' onChange={this.handleChange}/>
+
+                <div className='buttons has-addons'>
+                    <button className='button is-primary' onClick={this.handleSubmit}>Submit</button>
+                    <button className='button is-info' onClick={this.handleCancel}>Cancel</button>
+                    <button className='button is-danger' onClick={this.handleDelete}>Delete Post</button>
+                </div>
+
+            </div>
+        );
+    }
+}
+
+EditPost.propTypes = {
+    title: PropTypes.string,
+    content: PropTypes.string,
+    match: PropTypes.object,
+    callback: PropTypes.func,
+    history: PropTypes.object
+};
+
+export default withRouter(EditPost);
