b35a666b6dfa34bbd6e709f462b5701fb7f16a15
Momchil Marinov <mom4ilakis@gmail.com>
2020-03-12 17:22:10 +0200

    Reorganized imports. Added route for profile info

--- Web/Blog/src/app.jsx
+++ Web/Blog/src/app.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
+
 import Post from './Post';
 import HomePage from './PostsPreview';
 import Login from './Login';
 import CreatePost from './CreatePost';
-
+import Author from './Author';
 import api from './api';
 
-import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
 
 class App extends React.Component {
     constructor (props) {
@@ -46,11 +47,14 @@ class App extends React.Component {
                     {this.state.isAuthor && <Link className='button is-primary' to='/create-post/'>New Post</Link>}
                     <Link className='button is-info' to='/'>Home</Link>
                     {!this.state.logged && <Link className='button is-primary' to='/login/'>Log in</Link>}
+                    {this.state.logged && <Link className='button is-info' to='/author/'>Profile</Link>}
                     {this.state.logged && <Link className='button is-danger' onClick={this.handleUserLoggedOut} to='/'>Log out</Link>}
+
                 </div>
                 <Switch>
-                    <Route exact path='/create-post/'><CreatePost isAuthor={this.state.isAuthor}/></Route>
                     <Route exact path='/'><HomePage/></Route>
+                    <Route exact path='/author/' render={ (routeProps) => <Author {...routeProps} authorID={this.state.userID}/>}/>
+                    <Route exact path='/create-post/'><CreatePost isAuthor={this.state.isAuthor}/></Route>
                     <Route exact path='/login/'><Login callback={this.userLoggedIn}/></Route>
                     <Route exact path='/posts/:postID/' render={ (routeProps) => <Post {...routeProps} logged={this.state.logged} userID={this.state.userID}/>}/>
                     <Route exact path='/edit_post/' component={CreatePost}/>
