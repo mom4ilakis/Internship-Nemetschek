a66f1eba598ed929314008de23600632677c0e8e
Momchil Marinov <mom4ilakis@gmail.com>
2020-03-13 17:16:07 +0200

    Added delete function

--- Web/Blog/src/Reply.jsx
+++ Web/Blog/src/Reply.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+
 import api from './api';
-
-
-
+import utils from './utils';
 class Reply extends React.Component {
-
     state = {
         content: null,
         editing: false
@@ -21,24 +19,68 @@ class Reply extends React.Component {
     }
 
     handleDelete = () => {
-        api.delete(`/replies/${this.props.id}/`);
+        api.delete(`/replies/${this.props.id}/`)
+            .then(() => {
+                this.props.removeRep(this.props.id);
+            })
+            .catch(err => console.log(err));
+    }
+
+    handleSubmit = () => {
+        api.patch(`/replies/${this.props.id}/`, { content: this.state.content })
+            .then(() => {
+                const newReply = {
+                    content: this.state.content,
+                    author: this.props.author,
+                    date: this.props.date,
+                    id: this.props.id
+                };
+                this.props.updateRep(newReply);
+                this.handleEdit();
+            })
+            .catch(err => console.log(err));
+    }
+
+    handleCancel = () => {
+        this.setState({ content: this.props.content, editing: false });
+    }
+
+    editView = () => {
+        return (
+            <div className='box'>
+                <label className='label'>New reply</label>
+                <input id='content' className='input' onChange={this.handleTyping}/>
+                <div className='buttons are-centered'>
+                    <button className='button is-primary' onClick={this.handleSubmit}>Submit</button>
+                    <button className='button is-info' onClick={this.handleCancel}>Cancel</button>
+                </div>
+            </div>
+        );
+    }
+
+    display = () => {
+        return (
+            <div>
+                <p className='content is-medium'>{this.props.content}</p>
+                <div className='content is-small'>
+                    {this.props.author.username}
+                    <br/>
+                    {utils.formatTime(this.props.date)}
+                    <br/>
+                    {utils.formatDate(this.props.date)}
+                </div>
+                {(this.props.author.id === this.props.userID) &&
+                <div className='buttons are-centered'>
+                    <button className='button is-primary' onClick={this.handleEdit}>Edit</button>
+                    <button className='button is-danger' onClick={this.handleDelete}>Delete</button>
+                </div>}
+            </div>
+        );
     }
 
     render () {
         return (
-            <div>
-                <p>{this.props.content}</p>
-                <div className='contetn is-small'>
-                    {this.props.author.username}
-                    {this.props.date}
-                    {this.props.time}
-                </div>
-                {(this.props.content.author.id === this.props.userID) &&
-                <div className='buttons are-centered'>
-                    <button className='button is-primary' onClick={this.handleEdit}>Edit</button>
-                    <button className='button is-danger' >Delete</button>
-                </div>}
-            </div>
+            (this.state.editing && this.editView()) || (this.display())
         );
     }
 }
@@ -48,7 +90,9 @@ Reply.propTypes = {
     content: PropTypes.string,
     author: PropTypes.object,
     date: PropTypes.string,
-    time: PropTypes.string
+    id: PropTypes.number,
+    removeRep: PropTypes.func,
+    updateRep: PropTypes.func
 };
 
 export default Reply;
