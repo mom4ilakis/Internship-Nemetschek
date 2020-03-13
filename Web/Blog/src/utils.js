13976a26d5ee5262252763bba30824a7b6540cd3
Momchil Marinov <mom4ilakis@gmail.com>
2020-03-13 17:16:21 +0200

    Added validURL function

--- Web/Blog/src/utils.js
+++ Web/Blog/src/utils.js
@@ -15,7 +15,18 @@ const utils = {
             const minutes = date.getUTCMinutes() >= 10 ? date.getUTCMinutes() : '0' + date.getUTCMinutes();
             return `${hours}:${minutes}`;
         }
+    },
+    validURL: (str) => {
+        var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+          '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+          '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+          '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+          '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+          '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+        return !!pattern.test(str);
     }
 };
+
+
 
 export default utils;
