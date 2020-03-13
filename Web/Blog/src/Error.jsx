509895a86b0967458a43427a84ccb96d34123a5e
Momchil Marinov <mom4ilakis@gmail.com>
2020-03-13 17:15:38 +0200

    Innit commit

---
+++ Web/Blog/src/Error.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Error extends React.Component {
+    getInfo = () => {
+        return JSON.stringify(this.props.message);
+    };
+
+    render () {
+        return (
+            <div className='notification' onClick={this.props.callback}>{this.getInfo()}</div>
+        );
+    }
+}
+
+Error.propTypes = {
+    message: PropTypes.object,
+    callback: PropTypes.func
+}
+
+export default Error;
