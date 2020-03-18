70b5d2436a08603ef0ae0439f4c9b3629190704d
Momchil Marinov <mom4ilakis@gmail.com>
2020-03-18 17:33:35 +0200

    Added avatar and username render

--- Web/Blog/src/AuthorDisplay.jsx
+++ Web/Blog/src/AuthorDisplay.jsx
@@ -1,2 +1,16 @@
 import React from 'react';
 
+
+class AuthorDisplay extends React.Component {
+
+    render () {
+        return (
+            <div>
+                <img className='image is-48x48' src={this.props.avatar}/>
+                {this.props.username}
+            </div>
+        );
+    }
+}
+
+export default AuthorDisplay;
