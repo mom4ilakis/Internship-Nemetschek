300e82ef9a2b313db62ecbc6bef71914278d6878
Momchil Marinov <mom4ilakis@gmail.com>
2020-03-20 11:16:09 +0200

    Cleaned code

--- Web/Blog/src/index.jsx
+++ Web/Blog/src/index.jsx
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom';
 
 import './css/index-styles.scss';
 import Login from './Login';
-import App from './App';
+import App from './app';
 
 ReactDOM.render(
-    <div>
-        <App/>
-    </div>
-    ,
-    document.getElementById('root')
+  <div>
+    <App />
+  </div>,
+  document.getElementById('root'),
 );
